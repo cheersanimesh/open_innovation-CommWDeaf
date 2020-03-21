8b128c31303a43883a39cf314cd323986e559d13
shridhar-t <57321529+shridhar-t@users.noreply.github.com>
2020-03-21 15:32:32 +0530

    Add files via upload

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,34 +1,20 @@
 const express =require("express");
 const app =express();
+
 const extractFrames = require('ffmpeg-extract-frames')
 const fs = require('fs');
 const path = require('path');
 const axios= require("axios");
-
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath)
 var n=1;
-function frame_out() {
-    extractFrames({
-		input: 'blobby ('+n+').webm',
-		output: './frames/frames-%i.jpg',
-		offsets: [
-		  1000,
-		  2000,
-		  3000,
-		  4000,
-		  5000,
-		  6000,
-		  7000,
-		  8000,
-		  9000,
-		  10000,
-		]
-	  })
-	n++;
-	axios.post('http://0.0.0.0:5000/post',{
-        string:"num1",
-    })
-}
+const serverUrl='http://0.0.0.0:5000/';
+const http = axios.create({
+      baseURL:serverUrl,
+});
 app.post("/", function(req,res){
+	
 	const directory = './frames';
 
 fs.readdir(directory, (err, files) => {
@@ -40,11 +26,31 @@ fs.readdir(directory, (err, files) => {
     });
   }
 });
-setTimeout(frame_out,3000);
 
+	extractFrames({
+	  input: 'blobby ('+n+').webm',
+	  output: './frames/frames-%i.jpg',
+	  offsets: [
+		1000,
+		2000,
+		3000,
+		4000,
+		5000,
+		6000,
+		7000,
+		8000,
+		9000,
+		10000,
+	  ]
+	})
+	n++;
 	
+	http.post('/POST',{ //my server path
+        string:'num1', //irrelevant 
+	})
 	
 })
+
 app.listen(4000,function(){
     console.log("server on 4000");
 });
